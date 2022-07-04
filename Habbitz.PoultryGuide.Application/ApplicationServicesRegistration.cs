e82dbefc502f3665502fb082d52359b896f92fcd
anoldKupara <57469308+anoldKupara@users.noreply.github.com>
2022-07-04 15:05:15 +0200

    created ApplicationServicesRegistration

---
+++ Habbitz.PoultryGuide.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Habbitz.PoultryGuide.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Habbitz.PoultryGuide.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingProfile));
+        }
+    }
+}
