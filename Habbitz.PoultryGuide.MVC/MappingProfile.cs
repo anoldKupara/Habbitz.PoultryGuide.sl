916b267a34af93ffbfb7409ee1df068b6d20f9eb
anoldKupara <57469308+anoldKupara@users.noreply.github.com>
2022-08-05 10:54:00 +0200

    added the Mapping Profiles for the frontend

---
+++ Habbitz.PoultryGuide.MVC/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Habbitz.PoultryGuide.Application.DTOs.Feeds;
+using AutoMapper;
+using Habbitz.PoultryGuide.MVC.Models;
+
+namespace Habbitz.PoultryGuide.MVC
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateFeedDto, CreateFeedVM>().ReverseMap();
+        }
+    }
+}
---
+++ Habbitz.PoultryGuide.MVC/Models/FeedVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Habbitz.PoultryGuide.MVC.Models
+{
+    public class FeedVM : CreateFeedVM
+    {
+        public int Id { get; set; }
+    }
+
+    public class CreateFeedVM
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Purpose { get; set; }
+    }
+}
